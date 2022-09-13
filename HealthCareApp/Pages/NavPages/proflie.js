eadaba41bb53ad330ab0fd7d9c65d36f655a62ca
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-09-13 12:31:22 +0530

    bug fix

--- HealthCareApp/Pages/NavPages/proflie.js
+++ HealthCareApp/Pages/NavPages/proflie.js
@@ -18,6 +18,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    // backgroundColor: 'red',
+    backgroundColor: '#9ae3af',
   },
 });
