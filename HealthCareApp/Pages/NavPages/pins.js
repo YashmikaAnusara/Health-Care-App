33fbe98f0cbf5f9c2fae27148e90a844c6b04e19
Thivanka Saparamadu <86546750+thiva99@users.noreply.github.com>
2022-09-14 21:59:52 +0530

    updated

--- HealthCareApp/Pages/NavPages/pins.js
+++ HealthCareApp/Pages/NavPages/pins.js
@@ -1,20 +1,66 @@
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
-
+ import React from 'react';
+import {StyleSheet, Text, View, SafeAreaView, ScrollView} from 'react-native';
 export default function Pins({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>Pins Page</Text>
-      <Button title="Press Me" onPress={() => alert('wow You press me Pins')} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.Body}>
+        <ScrollView>
+          <View style={styles.ItemHeader}>
+            <Text
+              style={{
+                fontSize: 30,
+                fontWeight: 'bold',
+                color: 'green',
+                fontFamily: 'Arial',
+                marginTop: 20,
+              }}>
+              My Options
+            </Text>
+          </View>
+          <View style={styles.ItemBody}>
+            <View style={styles.RowWrapper}>
+              <Text>Hi</Text>
+            </View>
+            <View style={styles.RowWrapper}>
+              <Text>Hi</Text>
+            </View>
+            <View style={styles.RowWrapper}>
+              <Text>Hi</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.Footter}></View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'white',
     flex: 1,
+  },
+  Body: {
+    flex: 8,
+    backgroundColor: 'white',
+    flexDirection: 'column',
+  },
+  ItemHeader: {alignItems: 'center', justifyContent: 'center'},
+  ItemBody: {
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#5DB075',
+    padding: 15,
+  },
+
+  Footter: {
+    flex: 1.7,
+  },
+
+  RowWrapper: {
+    padding: 10,
+    width: "100%",
+    borderBottomWidth: 2,
+    borderColor: 'rgb(224, 224, 224)',
+    flexDirection:"row"
   },
 });
