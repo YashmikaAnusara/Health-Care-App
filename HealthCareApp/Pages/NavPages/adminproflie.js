47364521f1d38909a6702eb55a8fc1469cbf5a9f
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-09-14 20:36:29 +0530

    bug fix

---
+++ HealthCareApp/Pages/NavPages/adminproflie.js
@@ -0,0 +1,144 @@
+import React, {useState} from 'react';
+import {Image, StyleSheet, Text, View, ScrollView} from 'react-native';
+
+export default function Admin_proflie({navigation}) {
+  const [photo, setphoto] = useState(false);
+  const [posts, setposts] = useState(true);
+
+  const logoutHandler = () => {
+    navigation.navigate('Login');
+  };
+
+  const photohandler = () => {
+    setphoto(true);
+    setposts(false);
+  };
+  const postshandler = () => {
+    setposts(true);
+    setphoto(false);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.setting}>Setting</Text>
+        <Text style={styles.logout} onPress={logoutHandler}>
+          Logout
+        </Text>
+        <Text style={styles.topic}>Profile</Text>
+        <Image
+          source={require('../../Assets/pic.png')}
+          resizeMode="contain"
+          style={{
+            width: 200,
+            height: 200,
+            top: 10,
+            alignSelf: 'center',
+          }}
+        />
+        <Text style={styles.name}>Yashmika Saparamadu</Text>
+        <Text style={styles.subname}>A mantra goes here</Text>
+      </View>
+      {/* <Button
+        title="Press Me"
+        onPress={() => alert('wow You press me Proflie')}
+      /> */}
+    </View>
+    // </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    backgroundColor: '#ffffff',
+  },
+  box: {
+    width: '100%',
+    height: '30%',
+    backgroundColor: '#5DB075',
+    display: 'flex',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  topic: {
+    textAlign: 'center',
+    top: 0,
+    fontSize: 35,
+    color: 'white',
+    // alignContent: 'center',
+    // justifyContent: 'center',
+  },
+  setting: {
+    fontSize: 20,
+    color: 'white',
+    top: 20,
+    left: 10,
+  },
+  logout: {
+    left: 310,
+    fontSize: 20,
+    top: -4,
+    color: 'white',
+  },
+  name: {
+    alignSelf: 'center',
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  subname: {
+    alignSelf: 'center',
+    fontSize: 20,
+    color: 'black',
+  },
+  profilebox: {
+    marginTop: 9,
+    padding: 5,
+    width: '100%',
+    // height: '120%',
+    // backgroundColor: 'red',
+  },
+  profileboxtopic: {
+    width: '100%',
+    // height: '20%',
+    backgroundColor: '#E8E8E8',
+    borderRadius: 100,
+  },
+  profileboxsub1: {
+    paddingTop: 20,
+    width: '100%',
+    // height: '90%',
+    // backgroundColor: 'blue',
+  },
+  profileboxtopicsub1: {
+    fontSize: 20,
+    top: 12,
+    left: 70,
+    color: 'black',
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+    // backgroundColor:"red"
+  },
+  profileboxtopicsub1press: {
+    fontSize: 20,
+    top: 12,
+    left: 70,
+    color: 'black',
+  },
+  profileboxtopicsub2: {
+    fontSize: 20,
+    left: 260,
+    top: -15,
+    color: 'black',
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+  },
+  profileboxtopicsub2press: {
+    fontSize: 20,
+    left: 260,
+    top: -15,
+    color: 'black',
+  },
+});
--- HealthCareApp/Pages/navpage.js
+++ HealthCareApp/Pages/navpage.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+//user pages
 import Feed from './NavPages/Feed';
 import Proflie from './NavPages/proflie';
 import Stats from './NavPages/stats';
 import Pins from './NavPages/pins';
 
+//admin pages
+import Admin_proflie from './NavPages/adminproflie';
+import Admin_stats from './NavPages/adminstats';
+import Admin_feed from './NavPages/adminfeed';
+
 const Tab = createBottomTabNavigator();
 
-export default function Navpage() {
+export default function Navpage({route}) {
   return (
     <Tab.Navigator
       screenOptions={{
@@ -27,138 +33,248 @@ export default function Navpage() {
           ...styles.shadow,
         },
       }}>
-      <Tab.Screen
-        name="Feed"
-        component={Feed}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({focused}) => (
-            <View
-              style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                top: 5,
-              }}>
-              <Image
-                source={require('../Assets/Feed.png')}
-                resizeMode="contain"
+      {route.params.position === 'User' ? (
+        <Tab.Screen
+          name="Feed"
+          component={Feed}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+              <View
                 style={{
-                  width: 35,
-                  height: 35,
-                  tintColor: focused ? '#007AFF' : '#3C3C43',
-                }}
-              />
-              <Text
-                style={{
-                  color: 'black',
-                  fontSize: 12,
-                  //   textAlign:'center'
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  top: 5,
                 }}>
-                My Feed
-              </Text>
-            </View>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Stats"
-        component={Stats}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({focused}) => (
-            <View
-              style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                top: 5,
-              }}>
-              <Image
-                source={require('../Assets/My_Stats.png')}
-                resizeMode="contain"
+                <Image
+                  source={require('../Assets/Feed.png')}
+                  resizeMode="contain"
+                  style={{
+                    width: 35,
+                    height: 35,
+                    tintColor: focused ? '#007AFF' : '#3C3C43',
+                  }}
+                />
+                <Text
+                  style={{
+                    color: 'black',
+                    fontSize: 12,
+                    //   textAlign:'center'
+                  }}>
+                  My Feed
+                </Text>
+              </View>
+            ),
+          }}
+        />
+      ) : (
+        <Tab.Screen
+          name="Admin_feed"
+          component={Admin_feed}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+              <View
                 style={{
-                  width: 35,
-                  height: 35,
-                  tintColor: focused ? '#007AFF' : '#3C3C43',
-                }}
-              />
-              <Text
-                style={{
-                  color: 'black',
-                  fontSize: 12,
-                  //   textAlign:'center'
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  top: 5,
                 }}>
-                My Stats
-              </Text>
-            </View>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Pins"
-        component={Pins}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({focused}) => (
-            <View
-              style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                top: 5,
-              }}>
-              <Image
-                source={require('../Assets/pins.png')}
-                resizeMode="contain"
+                <Image
+                  source={require('../Assets/Feed.png')}
+                  resizeMode="contain"
+                  style={{
+                    width: 35,
+                    height: 35,
+                    tintColor: focused ? '#007AFF' : '#3C3C43',
+                  }}
+                />
+                <Text
+                  style={{
+                    color: 'black',
+                    fontSize: 12,
+                    //   textAlign:'center'
+                  }}>
+                  Feed
+                </Text>
+              </View>
+            ),
+          }}
+        />
+      )}
+      {route.params.position === 'User' ? (
+        <Tab.Screen
+          name="Stats"
+          component={Stats}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+              <View
                 style={{
-                  width: 35,
-                  height: 35,
-                  tintColor: focused ? '#007AFF' : '#3C3C43',
-                }}
-              />
-              <Text
-                style={{
-                  color: 'black',
-                  fontSize: 12,
-                  //   textAlign:'center'
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  top: 5,
                 }}>
-                My Pins
-              </Text>
-            </View>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={Proflie}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({focused}) => (
-            <View
-              style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                top: 5,
-              }}>
-              <Image
-                source={require('../Assets/profile.png')}
-                resizeMode="contain"
+                <Image
+                  source={require('../Assets/My_Stats.png')}
+                  resizeMode="contain"
+                  style={{
+                    width: 35,
+                    height: 35,
+                    tintColor: focused ? '#007AFF' : '#3C3C43',
+                  }}
+                />
+                <Text
+                  style={{
+                    color: 'black',
+                    fontSize: 12,
+                    //   textAlign:'center'
+                  }}>
+                  My Stats
+                </Text>
+              </View>
+            ),
+          }}
+        />
+      ) : (
+        <Tab.Screen
+          name="Admin_stats"
+          component={Admin_stats}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+              <View
                 style={{
-                  width: 35,
-                  height: 35,
-                  tintColor: focused ? '#007AFF' : '#3C3C43',
-                }}
-              />
-              <Text
-                style={{
-                  color: 'black',
-                  fontSize: 12,
-                  //   textAlign:'center'
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  top: 5,
                 }}>
-                My Profile
-              </Text>
-            </View>
-          ),
-        }}
-      />
+                <Image
+                  source={require('../Assets/My_Stats.png')}
+                  resizeMode="contain"
+                  style={{
+                    width: 35,
+                    height: 35,
+                    tintColor: focused ? '#007AFF' : '#3C3C43',
+                  }}
+                />
+                <Text
+                  style={{
+                    color: 'black',
+                    fontSize: 12,
+                    //   textAlign:'center'
+                  }}>
+                  Emp Stats
+                </Text>
+              </View>
+            ),
+          }}
+        />
+      )}
+      {route.params.position === 'User' ? (
+        <Tab.Screen
+          name="Pins"
+          component={Pins}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+              <View
+                style={{
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  top: 5,
+                }}>
+                <Image
+                  source={require('../Assets/pins.png')}
+                  resizeMode="contain"
+                  style={{
+                    width: 35,
+                    height: 35,
+                    tintColor: focused ? '#007AFF' : '#3C3C43',
+                  }}
+                />
+                <Text
+                  style={{
+                    color: 'black',
+                    fontSize: 12,
+                    //   textAlign:'center'
+                  }}>
+                  My Pins
+                </Text>
+              </View>
+            ),
+          }}
+        />
+      ) : null}
+      {route.params.position === 'User' ? (
+        <Tab.Screen
+          name="Profile"
+          component={Proflie}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+              <View
+                style={{
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  top: 5,
+                }}>
+                <Image
+                  source={require('../Assets/profile.png')}
+                  resizeMode="contain"
+                  style={{
+                    width: 35,
+                    height: 35,
+                    tintColor: focused ? '#007AFF' : '#3C3C43',
+                  }}
+                />
+                <Text
+                  style={{
+                    color: 'black',
+                    fontSize: 12,
+                    //   textAlign:'center'
+                  }}>
+                  My Profile
+                </Text>
+              </View>
+            ),
+          }}
+        />
+      ) : (
+        <Tab.Screen
+          name="Admin_proflie"
+          component={Admin_proflie}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+              <View
+                style={{
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  top: 5,
+                }}>
+                <Image
+                  source={require('../Assets/profile.png')}
+                  resizeMode="contain"
+                  style={{
+                    width: 35,
+                    height: 35,
+                    tintColor: focused ? '#007AFF' : '#3C3C43',
+                  }}
+                />
+                <Text
+                  style={{
+                    color: 'black',
+                    fontSize: 12,
+                    //   textAlign:'center'
+                  }}>
+                  My Profile
+                </Text>
+              </View>
+            ),
+          }}
+        />
+      )}
     </Tab.Navigator>
   );
 }
